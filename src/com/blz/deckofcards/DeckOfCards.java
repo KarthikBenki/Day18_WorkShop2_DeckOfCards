04c3df17002f6148aa58a5ac46f0fa8ac501082d
karthikbenki <karthikmc007@gmail.com>
2022-03-07 19:36:57 +0530

    Added: players class and number of players play the game logic

--- src/com/blz/deckofcards/DeckOfCards.java
+++ src/com/blz/deckofcards/DeckOfCards.java
@@ -1,5 +1,8 @@
 package com.blz.deckofcards;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
  * @purpose :Deck of cards simulation
  * @file : DeckOfCards.java
@@ -9,6 +12,8 @@ public class DeckOfCards {
     static String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
     static String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     static String[] deckOfCard = new String[suit.length * rank.length];
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Player> playerList = new ArrayList<>();
 
     /*
      * @purpose : Ability to Arrange Deck of cards uniquely
@@ -38,12 +43,37 @@ public class DeckOfCards {
         }
         System.out.println();
     }
-
+    /*
+     * @purpose : Ability to set player order
+     * @param : No parameters
+     * @function : Check for Number of players to play the game
+     * @return : No return Value;
+     */
+    public void numOfPlayers() {
+        System.out.println("Enter the number of players,range must be from 2 to 4");
+        int playerCount = scanner.nextInt();
+        if (playerCount >= 2 && playerCount <= 4) {
+            Player player = new Player();
+            for(int i =0;i<playerCount;i++){
+                System.out.println("Enter player "+(i+1)+" name");
+                player.setPlayerName(scanner.next());
+                player.setPlayerNo(i+1);
+                playerList.add(player);
+            }
+            System.out.println(playerCount + " players will play the game");
+        } else {
+            System.out.println("please enter the correct range");
+            this.numOfPlayers();
+            scanner.close();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to deck of cards simulation");
         DeckOfCards deckOfCards = new DeckOfCards();
         deckOfCards.getUniqueCards();
         System.out.println("Unique cards are");
         deckOfCards.printArray(deckOfCard);
+        deckOfCards.numOfPlayers();
+        System.out.println(playerList);
     }
 }
