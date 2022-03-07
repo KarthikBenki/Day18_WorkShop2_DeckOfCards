112f3648bf88a03c26a9cabbde3764d2a343d410
karthikbenki <karthikmc007@gmail.com>
2022-03-07 18:52:46 +0530

    Added: Welcome Message

---
+++ src/com/blz/deckofcards/DeckOfCards.java
@@ -0,0 +1,12 @@
+package com.blz.deckofcards;
+
+/*
+ * @purpose :Deck of cards simulation
+ * @file : DeckOfCards.java
+ * @author : Karthik M C
+ */
+public class DeckOfCards {
+    public static void main(String[] args) {
+        System.out.println("Welcome to deck of cards simulation");
+    }
+}
